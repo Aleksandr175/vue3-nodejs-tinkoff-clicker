2aa1a656219d58ce00427fa9ccf8c405bf17cfc2
Aleksandr Pogrebitskiy <a.pogr@yandex.ru>
2021-04-04 15:06:05 +0300

    Init vue and node js

---
+++ vue/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="100">
+
+    <div>Курс: <span>1 USD : 75 RUB</span></div>
+    <div>Баланс: <span>123.33 USD</span></div>
+
+    <div>
+      <button class="btn">Поменять</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  setup() {
+    console.log('Vue setup');
+  }
+}
+</script>
+
+<style>
+.app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
